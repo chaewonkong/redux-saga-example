26588d761f382302a1e1fe712495bf0c51aa144c
Kong Chae Won <chaewonkong@gmail.com>
2020-01-20 16:26:00 +0900

    feat: add saga to delay score up/down button to 2 sec

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./stores/reducers";
+import rootSaga from "./sagas/saga";
+import createSagaMiddleware from "redux-saga";
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
